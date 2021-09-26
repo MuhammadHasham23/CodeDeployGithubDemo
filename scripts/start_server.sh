ff46e4b78e0b427a2d18a63fb173af55c3452a06
MuhammadHasham23 <muahmmad.hasham.2311@gmail.com>
2021-09-26 22:43:51 +0200

    fix dir issue

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #give permission for everything in the express-app directory
-sudo chmod -R 777 /home/ec2-user/express-app
+sudo chmod -R 777 /var/www/html
 
 #navigate into our working directory where we have all our github files
-cd /home/ec2-user/express-app
+cd /var/www/html
 
 #add npm and node to path
 export NVM_DIR="$HOME/.nvm"
